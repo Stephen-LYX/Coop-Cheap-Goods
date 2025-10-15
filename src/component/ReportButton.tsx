2c2cd3cf071e2801a7b887247a5a1084ce3754d6
gthubuser-code <githubuser820@gmail.com>
2025-10-14 22:37:35 -0400

    Update ReportButton.tsx

--- src/component/ReportButton.tsx
+++ src/component/ReportButton.tsx
@@ -1,42 +1,52 @@
 "use client";
 
 import { createClient } from "@/utils/supabase/client";
+import Link from "next/link";
 import { FormEvent, useState } from "react";
 
 export function ReportButton({ itemID }) {
   const [text, setText] = useState<string>("Report Item");
   const [disabled, setDisabled] = useState<boolean>(false);
+  const [redirect, setRedirect] = useState<boolean>(false);
 
   //report item
-  async function reportItem(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  async function reportItem() {
     setDisabled(true);
+
     setText("Reporting...");
     try {
       const supabase = await createClient();
+
+      //check is user is logged in
       const {
         data: { user },
       } = await supabase.auth.getUser();
-
-      //check if user already reported this item
-      const { data: reports } = await supabase
-        .from("reports")
-        .select()
-        .eq("item", itemID)
-        .eq("profile", user.id);
-      if (reports.length > 0) {
-        setText("Item Already Reported");
+      if (user == null) {
+        setRedirect(true);
+        setText("Login to Report");
+        setDisabled(false);
       } else {
-        const { error } = await supabase.from("reports").insert({
-          item: itemID,
-          profile: user.id,
-        });
-        if (error) {
-          console.log(error);
-          setDisabled(false);
-          setText("Error Reporting Item, Try Again");
+        //check if user already reported this item
+        const { data: reports } = await supabase
+          .from("reports")
+          .select()
+          .eq("item", itemID)
+          .eq("profile", user.id);
+        if (reports.length > 0) {
+          setText("Item Already Reported");
         } else {
-          setText("Item Reported");
+          //report item
+          const { error } = await supabase.from("reports").insert({
+            item: itemID,
+            profile: user.id,
+          });
+          if (error) {
+            console.log(error);
+            setDisabled(false);
+            setText("Error Reporting Item, Try Again");
+          } else {
+            setText("Item Reported");
+          }
         }
       }
     } catch (error) {
@@ -46,10 +56,21 @@ export function ReportButton({ itemID }) {
     }
   }
 
-  return (
-    <form onSubmit={reportItem}>
-      <input type="text" value={itemID} hidden />
+  if (redirect) {
+    return (
+      <Link
+        href={"/login"}
+        className={
+          "w-full block py-2 my-4 border bg-red-600 text-center text-white w-auto hover:bg-red-700 rounded"
+        }
+      >
+        {text}
+      </Link>
+    );
+  } else {
+    return (
       <button
+        onClick={reportItem}
         disabled={disabled}
         className={
           disabled
@@ -59,6 +80,6 @@ export function ReportButton({ itemID }) {
       >
         {text}
       </button>
-    </form>
-  );
+    );
+  }
 }
