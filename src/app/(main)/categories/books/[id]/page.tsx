5a7aa1e6ae6af5bb4e7c5fe68d60e364346375a8
gthubuser-code <githubuser820@gmail.com>
2026-02-09 11:05:22 -0500

    Item listing page for all categories

--- src/app/(main)/categories/books/[id]/page.tsx
+++ src/app/(main)/categories/books/[id]/page.tsx
@@ -44,7 +44,7 @@ export default async function Item({
 
     //get item from database
     const { data: item } = await supabase
-      .from("items_other") // select from other_items table
+      .from("items_books") // select from other_items table
       .select() // select all columns
       .eq("id", itemID) // where the item id = itemID
       .single() // return data as a single object
@@ -122,9 +122,9 @@ export default async function Item({
                 Category:{" "}
                 <Link
                   className="hover:text-blue-700"
-                  href={`/categories/other`}
+                  href={`/categories/books`}
                 >
-                  Other
+                  Books
                 </Link>
               </p>
               <Link
--- src/app/(main)/categories/musical-instruments/[id]/page.tsx
+++ src/app/(main)/categories/musical-instruments/[id]/page.tsx
@@ -44,7 +44,7 @@ export default async function Item({
 
     //get item from database
     const { data: item } = await supabase
-      .from("items_other") // select from other_items table
+      .from("items_musical_instruments") // select from other_items table
       .select() // select all columns
       .eq("id", itemID) // where the item id = itemID
       .single() // return data as a single object
@@ -122,9 +122,9 @@ export default async function Item({
                 Category:{" "}
                 <Link
                   className="hover:text-blue-700"
-                  href={`/categories/other`}
+                  href={`/categories/musical-instruments`}
                 >
-                  Other
+                  Musical Instruments
                 </Link>
               </p>
               <Link
@@ -145,6 +145,7 @@ export default async function Item({
               <h2 className="font-bold text-heading text-xl">About</h2>
               <ul className="list-disc list-inside">
                 <li>Condition: {item.condition ? item.condition : "NA"}</li>
+                <li>Brand: {item.brand ? item.brand : "NA"}</li>
               </ul>
               <hr className="my-4"></hr>
               <h2 className="font-bold text-heading text-xl">Seller</h2>
